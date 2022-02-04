feeb0e81e5f6cfcd07842101d24c42060979b317
JooYoo <jooyoo@outlook.com>
2022-02-04 19:09:32 +0100

    Initial Commit

---
+++ Ninone/NinoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NinoneApp.swift
+//  Ninone
+//
+//  Created by Yu on 2022/2/4.
+//
+
+import SwiftUI
+
+@main
+struct NinoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
