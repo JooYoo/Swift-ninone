c29408f10b604772c8f967ff89fc235f7d2101e7
JooYoo <jooyoo@outlook.com>
2022-02-07 19:47:30 +0100

    refactor: Rps add ViewModel

---
+++ Ninone/Apps/RockPaperScissors/RpsView.swift
@@ -0,0 +1,103 @@
+//
+//  RockPaperScissorsView.swift
+//  Ninone
+//
+//  Created by Yu on 2022/1/29.
+//
+
+import SwiftUI
+
+struct RockPaperScissorsView: View {
+    @StateObject var vm = RpsViewModel()
+    
+    var body: some View {
+       
+        ZStack{
+            // bg-color
+            switch vm.resState {
+            case .win:
+                Color(red: 0.02, green: 0.77, blue: 0.42)
+                    .ignoresSafeArea()
+            case .lose:
+                Color(red: 1.00, green: 0.42, blue: 0.51)
+                    .ignoresSafeArea()
+            case .deuce:
+                Color(red: 0.20, green: 0.67, blue: 0.88)
+                    .ignoresSafeArea()
+            }
+            
+            VStack{
+                Text("Round: \(vm.round)/\(vm.expectRoundAmount)")
+                    .font(.system(size: 20))
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                Text(vm.rps[vm.aiChoiceIdx])
+                    .font(.system(size: 120))
+                    .padding(.vertical,20)
+                    .padding(.horizontal,80)
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                
+                Text("You should \(vm.shouldWin ? "WIN" : "LOSE")")
+                    .font(.system(size: 40))
+                    .fontWeight(.heavy)
+                
+                Spacer()
+                
+                HStack{
+                    ForEach(vm.rps, id: \.self) { item in
+                        Button(item){
+                            vm.userTapped("\(item)")
+                        }
+                    }
+                }
+                .font(.system(size: 90))
+                .buttonStyle(.bordered)
+                
+                Text("SCORE: \(vm.score)")
+                    .font(.system(size: 30))
+                    .fontWeight(.semibold)
+                    .padding(.top, 20)
+                
+            }
+            .padding()
+            .foregroundColor(.white)
+            
+        }
+        .navigationBarTitle("Ro.Pa.Sci")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar{
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button{
+                    vm.showingSheet = true
+                }label: {
+                    Label("About", systemImage: "info.circle")
+                        .foregroundColor(.white)
+                }
+            }
+        }
+        .sheet(isPresented: $vm.showingSheet){
+            // TODO: create new View to introduce the app
+            Text("About this MiniApp")
+        }
+        .alert(vm.alerTitle, isPresented: $vm.showingAlert) {
+            Button("Ok"){
+                vm.restart()
+            }
+        } message: {
+            Text("Your final score is \(vm.score)")
+        }
+    }
+}
+
+struct RockPaperScissorsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{        
+            RockPaperScissorsView()
+        }
+    }
+}
---
+++ Ninone/Apps/RockPaperScissors/RpsViewModel.swift
@@ -0,0 +1,103 @@
+//
+//  RpsViewModel.swift
+//  Ninone
+//
+//  Created by Yu on 2022/2/7.
+//
+
+import Foundation
+
+class RpsViewModel: ObservableObject {
+    var rps = ["✊🏼", "✋🏼", "✌🏼"]
+    enum res {
+        case win
+        case lose
+        case deuce
+    }
+    let expectRoundAmount = 4
+    
+    @Published var aiChoiceIdx = Int.random(in: 0...2)
+    @Published var shouldWin = Bool.random()
+    @Published var resState = res.deuce
+    @Published var score = 0
+    @Published var round = 1
+    @Published var showingAlert = false
+    @Published var alerTitle = "Game Over"
+    @Published var showingSheet = false
+    
+    func userTapped(_ userChoosed: String){
+        
+        let aiRes = rps[aiChoiceIdx]
+        
+        switch aiRes {
+        case "✊🏼":
+            if userChoosed == "✋🏼" {
+                userShouldWinHandler()
+            }else if(userChoosed == "✌🏼"){
+                userShouldLoseHandler()
+            }else{
+                resState = .deuce
+            }
+        case "✌🏼":
+            if userChoosed == "✊🏼" {
+                userShouldWinHandler()
+            } else if (userChoosed == "✋🏼"){
+                userShouldLoseHandler()
+            }else{
+                resState = .deuce
+            }
+        case "✋🏼":
+            if userChoosed == "✌🏼" {
+                userShouldWinHandler()
+            }else if(userChoosed == "✊🏼"){
+                userShouldLoseHandler()
+            }else{
+                resState = .deuce
+            }
+        default: ()
+        }
+        
+        nextRound()
+    }
+    
+    func userShouldWinHandler(){
+        if shouldWin {
+            score += 1
+            resState = .win
+        }else{
+            score -= 1
+            resState = .lose
+        }
+    }
+    
+    func userShouldLoseHandler(){
+        if !shouldWin {
+            score += 1
+            resState = .win
+        }else{
+            score -= 1
+            resState = .lose
+        }
+    }
+    
+    func nextRound(){
+        // check if end
+        if round == expectRoundAmount {
+            // showing alert
+            showingAlert = true
+        } else {
+            // next round
+            round += 1
+            aiChoiceIdx = Int.random(in: 0...2)
+            shouldWin = Bool.random()
+        }
+    }
+    
+    func restart(){
+        round = 1
+        score = 0
+        resState = .deuce
+        aiChoiceIdx = Int.random(in: 0...2)
+        shouldWin = Bool.random()
+    }
+}
